b8821e847ad55507524aa22a21448a38dbce1eeb
Sviatoslav <43699040+Felis1@users.noreply.github.com>
2020-12-18 22:44:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf('1446411020:AAHhFjr1onQ6dIyBEAEbDUPITKdY1tfYPIw');
-bot.start((ctx) => {
-    ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
-    ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    const bot = new Telegraf('1446411020:AAHhFjr1onQ6dIyBEAEbDUPITKdY1tfYPIw');
+    bot.start((ctx) => {
+        ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
+        ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
+    });
 });
 
 bot.launch();
