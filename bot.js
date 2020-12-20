76ca1f0ded772ae064567f01560c33dcf668a8c2
Felis1 <distoxi@gmail.com>
2020-12-20 20:00:20 +0200

    first bot commit dyno awake

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const { Telegraf } = require('telegraf');
+const { wakeDyno } = require('heroku-keep-awake');
 const express = require('express');
 const expressApp = express()
 const bot = new Telegraf('1446411020:AAHhFjr1onQ6dIyBEAEbDUPITKdY1tfYPIw');
 const PORT = process.env.PORT || 3000;
+const DYNO_URL = 'https://git.heroku.com/catacademy-bot.git';
 expressApp.listen(PORT, () => {
+    wakeDyno(DYNO_URL);
     bot.start((ctx) => {
         ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
         ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
