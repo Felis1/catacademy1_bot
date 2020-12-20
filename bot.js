73948a19c1f4b98d92c6d076d36b10ffaccb4577
Sviatoslav <43699040+Felis1@users.noreply.github.com>
2020-12-20 22:43:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,9 @@ const { wakeDyno } = require('heroku-keep-awake');
 const express = require('express');
 const expressApp = express()
 const bot = new Telegraf('1446411020:AAHhFjr1onQ6dIyBEAEbDUPITKdY1tfYPIw');
-const PORT = process.env.PORT || 3000;
-const DYNO_URL = 'https://catacademy-bot.herokuapp.com/';
-expressApp.listen(PORT, () => {
-    wakeDyno(DYNO_URL);
-    bot.start((ctx) => {
-        ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
-        ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
-    });
+bot.start((ctx) => {
+      ctx.reply('Send us the screenshot 📄 of your payment 💰 and wait for further instructions. We will send them in next few 🕐 hours. Thank you very much, and will see you on the Course soon!😻😽');
+      ctx.replyWithSticker('CAACAgIAAxkBAAKG5F_cbrrw__ecLqMM6mBqQ06l40fOAAIIAAPp2BMoePnrnGhu-XEeBA');
 });
 
 bot.launch();
